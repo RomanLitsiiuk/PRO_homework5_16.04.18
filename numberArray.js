2fefbb1b3197a469006e17af0864a61903293860
Roman Litsiiuk <alexandrovich2015@gmail.com>
2018-04-28 19:56:14 +0300

    numberArray script was modified

--- numberArray.js
+++ numberArray.js
@@ -1,23 +1,25 @@
 function createNumArray() {
     var randomArray = [];
-    var raiseOrder;
+    var raiseOrder = true;
     var i;
 
     for (i = 0; i < 5; i++) {
         randomArray.push( +(Math.random() * 10).toFixed(0) );
     }
 
-    console.log(randomArray);
-
-    raiseOrder = true;
     for (i = 0; i < randomArray.length; i++) {
         if (randomArray[i] > randomArray[i+1] || randomArray[i] === randomArray[i+1]) {
             raiseOrder = false;
         }
     }
 
-    raiseOrder ? console.log('Строго возрастающая последовательность') :
+    console.log(randomArray);
+
+    if (raiseOrder) {
+        console.log('Строго возрастающая последовательность');
+    } else {
         console.log('Строго возрастающая последовательность не соблюдена');
+    }
 }
 
 createNumArray();
